aa90441111ae45f501bc79d2b433b0fbce90899b
Jason Bell <jsbell9@gmail.com>
2022-09-27 21:59:02 -0500

    Updated scrape script

--- scrape.sh
+++ scrape.sh
@@ -1,4 +1,6 @@
-tickers=$(curl -s "https://allokate.ai/api/market/earnings" | jq -r ".data[].ticker")
+tickers=$(curl -s "https://app.allokate.ai/api/market/earnings" | jq -r ".data[].ticker")
+
+echo $tickers
 
 for ticker in $tickers; do 
     python sickle.py company $ticker;
